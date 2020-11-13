4948a7a639cd1f0a51ca93ece9328b260bdf7242
jishnu-coder <jishnumohan481@gmail.com>
2020-11-13 13:08:25 +0530

    second commit

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,24 +2,24 @@ import React,{useState} from 'react';
 import {connect} from 'react-redux';
 import axios from 'axios';
 const About = (props) => {
-  const [data,setData]=useState({
-    lon:"",
-    lat:""
-  })
-  axios.get("http://localhost:3001")
-  .then(res => {
-                  console.log(res.data)
-                  setData(res.data.coord)
-                  // console.log(data);
-                  })
-  .catch(error => console.log(error))
+  // const [data,setData]=useState({
+  //   lon:"",
+  //   lat:""
+  // })
+  // axios.get("http://localhost:3001")
+  // .then(res => {
+  //                 console.log(res.data)
+  //                 setData(res.data.coord)
+  //                 // console.log(data);
+  //                 })
+  // .catch(error => console.log(error))
   // console.log(props.posts);
   return <>
-  <h1> about </h1>
+  {/* <h1> about </h1>
   <img src="https://wallpapercave.com/wp/acHJ2Wr.jpg"
              height="150px" width="105px" alt="nothing to show"  />
 
-  <p>{data.lon}</p>
+  <p>{data.lon}</p> */}
   </>
 }
 const mapStateToProps=(state) => {
